667b5203d82d09ed55226e17b651237a2f16f86b
twishamanvar651-ctrl <twishamanvar651@gmail.com>
2026-09-23 01:08:40 +0530

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,8 @@
+export const metadata = { title: 'Astrosha - Premium AI Astrology' }
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{margin:0}}>{children}</body>
+    </html>
+  )
+}
